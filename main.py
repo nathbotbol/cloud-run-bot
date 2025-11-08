706ec75a433f536d2739145c2bb4d14e9ecae3e8
Nathan Botbol <nathbotbol@gmail.com>
2025-11-08 18:47:53 +0100

    chore: new strategy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import os
+import time
 import subprocess
 
 app = Flask(__name__)
@@ -10,34 +11,20 @@ INSTANCE = "instance-20251102-164456"
 
 @app.route("/", methods=["POST"])
 def run_bot():
-    # 1. Allumer la VM
+
+    # 1. start VM
     subprocess.run([
         "gcloud", "compute", "instances", "start",
         INSTANCE, "--zone", ZONE, "--project", PROJECT
     ], check=True)
 
-    # 2. Attendre que la VM démarre (optionnel)
-    import time
-    time.sleep(90)  # Ajuste si nécessaire
+    # 2. on laisse la VM booter + exécuter son startup script
+    time.sleep(240)
 
-    # 3. Exécuter le script sur la VM
+    # 3. (NE SURTOUT PAS SSH)
+    # ton run_bot.sh fera shutdown lui même
 
-    subprocess.run([
-        "gcloud", "compute", "ssh",
-        f"nathan@{INSTANCE}",
-        "--zone", ZONE,
-        "--project", PROJECT,
-        "--command", f"BASE_URL='{os.environ['BASE_URL']}' USERNAME='{os.environ['USERNAME']}' PASSWORD='{os.environ['PASSWORD']}' TARGET_START='{os.environ['TARGET_START']}' TARGET_END='{os.environ['TARGET_END']}' TARGET_SUBJECT='{os.environ['TARGET_SUBJECT']}' TARGET_COACH='{os.environ['TARGET_COACH']}' PEOPLE='{os.environ['PEOPLE']}' bash ~/bot-cycling/scripts/run_bot.sh"
-    ])
-
-    # 4. Éteindre la VM
-    subprocess.run([
-        "gcloud", "compute", "instances", "stop",
-        INSTANCE, "--zone", ZONE, "--project", PROJECT
-    ], check=True)
-
-    return "Bot exécuté et VM éteinte", 200
-
+    return "OK déclenché", 200
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
